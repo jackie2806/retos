3b83b0bf67f8b7e7098a3f813f4d5a3aaec0fa91
jackie2806 <jaishackeline@gmail.com>
2023-07-01 23:45:47 -0500

    explicación de la solución

--- e-1.js
+++ e-1.js
@@ -12,3 +12,13 @@ function multiply (number){
 }
 
 console.log(multiply(3));
+
+/*Explicación de la resolución*/
+
+/*
+ 1. Crear una función que tenga como parámetro el número del cual queremos hallar su tabla de multiplicar completa.
+ 2. Crear una variable dentro de la función que nos permita almacernar cada iteración, como vamos a almacernar valores concatenados, debemos iniciarla como un string vacío (comita sin espacio).
+ 3. Creamos un for para iterar cada uno de los valores que tenemos que multiplicar.
+ 4. La variable que fue declarada fuera del for, la inicializamos concatenando cada uno de los valores según el ciclo.
+ 5. Retornamos la variable que contiene el resultado final
+*/
