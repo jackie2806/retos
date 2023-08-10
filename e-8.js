3c7b26931deda3bc4d46b7b412151bbbc306b2ad
jackie2806 <jaishackeline@gmail.com>
2023-08-10 11:30:38 -0500

    otra solución

--- e-8.js
+++ e-8.js
@@ -2,15 +2,30 @@
 
 function invertirNumero(numero){
    let stringNumber = String(numero);
+
     let reverseNumber = stringNumber.split('').reverse().join('');
-
-    console.log(reverseNumber)
-    
-
+    //console.log(reverseNumber)    
     let newNumber = Number(reverseNumber);
-
 
     return newNumber;
 }
 
 console.log(invertirNumero(50100));
+
+// otra solución
+
+function invertirNumeros(numero){
+
+    let invertido = parseInt(
+                    numero
+                    .toString()
+                    .split('')
+                    .reverse()
+                    .join('')
+
+    ) * Math.sign(numero);
+
+    return invertido;
+}
+
+console.log(invertirNumeros(-15))
